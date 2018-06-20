bcb5ad70f0e4ebe506a5dbb88a302e1985fe037c
Kateryna Pynka <34662518+katedoctor@users.noreply.github.com>
2018-06-20 15:10:33 +0300

    Add files via upload

---
+++ homework_2/task_2/app.js
@@ -0,0 +1,356 @@
+class UserList {
+  constructor() {
+    this.listOfUsers = [{
+      "id": 1,
+      "first_name": "Abner",
+      "last_name": "Glaisner",
+      "gender": "Male",
+      "language": "Tswana"
+    }, {
+      "id": 2,
+      "first_name": "Hillyer",
+      "last_name": "Ashall",
+      "gender": "Male",
+      "language": "Oriya"
+    }, {
+      "id": 3,
+      "first_name": "Wait",
+      "last_name": "Dobrowolny",
+      "gender": "Male",
+      "language": "Chinese"
+    }, {
+      "id": 4,
+      "first_name": "Mayor",
+      "last_name": "Fumagall",
+      "gender": "Male",
+      "language": "Kashmiri"
+    }, {
+      "id": 5,
+      "first_name": "Winifield",
+      "last_name": "Klewer",
+      "gender": "Male",
+      "language": "Moldovan"
+    }, {
+      "id": 6,
+      "first_name": "Nanette",
+      "last_name": "Jagiela",
+      "gender": "Female",
+      "language": "New Zealand Sign Language"
+    }, {
+      "id": 7,
+      "first_name": "Davidson",
+      "last_name": "Orry",
+      "gender": "Male",
+      "language": "Tsonga"
+    }, {
+      "id": 8,
+      "first_name": "Laurianne",
+      "last_name": "Solman",
+      "gender": "Female",
+      "language": "Moldovan"
+    }, {
+      "id": 9,
+      "first_name": "Kent",
+      "last_name": "Imesen",
+      "gender": "Male",
+      "language": "Greek"
+    }, {
+      "id": 10,
+      "first_name": "Antonella",
+      "last_name": "Balston",
+      "gender": "Female",
+      "language": "Quechua"
+    }, {
+      "id": 11,
+      "first_name": "Kim",
+      "last_name": "Simko",
+      "gender": "Female",
+      "language": "Indonesian"
+    }, {
+      "id": 12,
+      "first_name": "Christiane",
+      "last_name": "Spurman",
+      "gender": "Female",
+      "language": "Mongolian"
+    }, {
+      "id": 13,
+      "first_name": "Shana",
+      "last_name": "Yellowley",
+      "gender": "Female",
+      "language": "Amharic"
+    }, {
+      "id": 14,
+      "first_name": "Adolpho",
+      "last_name": "Shepland",
+      "gender": "Male",
+      "language": "Oriya"
+    }, {
+      "id": 15,
+      "first_name": "Caesar",
+      "last_name": "Burdis",
+      "gender": "Male",
+      "language": "Malay"
+    }, {
+      "id": 16,
+      "first_name": "Deanna",
+      "last_name": "Goldsmith",
+      "gender": "Female",
+      "language": "Lao"
+    }, {
+      "id": 17,
+      "first_name": "Renata",
+      "last_name": "Baudins",
+      "gender": "Female",
+      "language": "Spanish"
+    }, {
+      "id": 18,
+      "first_name": "Phil",
+      "last_name": "Harle",
+      "gender": "Male",
+      "language": "Nepali"
+    }, {
+      "id": 19,
+      "first_name": "Cecilia",
+      "last_name": "Linnock",
+      "gender": "Female",
+      "language": "Telugu"
+    }, {
+      "id": 20,
+      "first_name": "Geri",
+      "last_name": "Lagde",
+      "gender": "Male",
+      "language": "Telugu"
+    }, {
+      "id": 21,
+      "first_name": "Etti",
+      "last_name": "Gibson",
+      "gender": "Female",
+      "language": "Afrikaans"
+    }, {
+      "id": 22,
+      "first_name": "Coleman",
+      "last_name": "Bodell",
+      "gender": "Male",
+      "language": "Irish Gaelic"
+    }, {
+      "id": 23,
+      "first_name": "Lazarus",
+      "last_name": "Grzegorzewski",
+      "gender": "Male",
+      "language": "Zulu"
+    }, {
+      "id": 24,
+      "first_name": "Korella",
+      "last_name": "Entissle",
+      "gender": "Female",
+      "language": "Fijian"
+    }, {
+      "id": 25,
+      "first_name": "Toby",
+      "last_name": "Stodd",
+      "gender": "Female",
+      "language": "Italian"
+    }, {
+      "id": 26,
+      "first_name": "Genvieve",
+      "last_name": "Papps",
+      "gender": "Female",
+      "language": "Malayalam"
+    }, {
+      "id": 27,
+      "first_name": "Kristy",
+      "last_name": "Tesdale",
+      "gender": "Female",
+      "language": "Czech"
+    }, {
+      "id": 28,
+      "first_name": "Chance",
+      "last_name": "Plenty",
+      "gender": "Male",
+      "language": "Macedonian"
+    }, {
+      "id": 29,
+      "first_name": "Trueman",
+      "last_name": "Lazare",
+      "gender": "Male",
+      "language": "Kazakh"
+    }, {
+      "id": 30,
+      "first_name": "Rosemarie",
+      "last_name": "Avramovic",
+      "gender": "Female",
+      "language": "Burmese"
+    }, {
+      "id": 31,
+      "first_name": "Aeriel",
+      "last_name": "Maybey",
+      "gender": "Female",
+      "language": "Bengali"
+    }, {
+      "id": 32,
+      "first_name": "Emmerich",
+      "last_name": "Menhci",
+      "gender": "Male",
+      "language": "Mongolian"
+    }, {
+      "id": 33,
+      "first_name": "Antoine",
+      "last_name": "Rait",
+      "gender": "Male",
+      "language": "Maltese"
+    }, {
+      "id": 34,
+      "first_name": "Vernor",
+      "last_name": "Devaney",
+      "gender": "Male",
+      "language": "Tamil"
+    }, {
+      "id": 35,
+      "first_name": "Giordano",
+      "last_name": "Thornbarrow",
+      "gender": "Male",
+      "language": "Malayalam"
+    }, {
+      "id": 36,
+      "first_name": "Iolande",
+      "last_name": "Goman",
+      "gender": "Female",
+      "language": "Sotho"
+    }, {
+      "id": 37,
+      "first_name": "Hobey",
+      "last_name": "Llywarch",
+      "gender": "Male",
+      "language": "Bengali"
+    }, {
+      "id": 38,
+      "first_name": "Sarge",
+      "last_name": "Lumbley",
+      "gender": "Male",
+      "language": "Polish"
+    }, {
+      "id": 39,
+      "first_name": "Lucila",
+      "last_name": "Daskiewicz",
+      "gender": "Female",
+      "language": "Mongolian"
+    }, {
+      "id": 40,
+      "first_name": "Odelinda",
+      "last_name": "Gambell",
+      "gender": "Female",
+      "language": "Tok Pisin"
+    }, {
+      "id": 41,
+      "first_name": "Terry",
+      "last_name": "Keeley",
+      "gender": "Female",
+      "language": "Tok Pisin"
+    }, {
+      "id": 42,
+      "first_name": "Esme",
+      "last_name": "Wallenger",
+      "gender": "Male",
+      "language": "Norwegian"
+    }, {
+      "id": 43,
+      "first_name": "Lilian",
+      "last_name": "Bonnick",
+      "gender": "Female",
+      "language": "Pashto"
+    }, {
+      "id": 44,
+      "first_name": "Marietta",
+      "last_name": "Kleisle",
+      "gender": "Female",
+      "language": "Ndebele"
+    }, {
+      "id": 45,
+      "first_name": "Stern",
+      "last_name": "Guare",
+      "gender": "Male",
+      "language": "Aymara"
+    }, {
+      "id": 46,
+      "first_name": "Gill",
+      "last_name": "Skelington",
+      "gender": "Female",
+      "language": "Belarusian"
+    }, {
+      "id": 47,
+      "first_name": "Celie",
+      "last_name": "Corington",
+      "gender": "Female",
+      "language": "Quechua"
+    }, {
+      "id": 48,
+      "first_name": "Aloin",
+      "last_name": "Corkan",
+      "gender": "Male",
+      "language": "Icelandic"
+    }, {
+      "id": 49,
+      "first_name": "Renell",
+      "last_name": "Allain",
+      "gender": "Female",
+      "language": "Irish Gaelic"
+    }, {
+      "id": 50,
+      "first_name": "Richy",
+      "last_name": "Worwood",
+      "gender": "Male",
+      "language": "Zulu"
+    }]
+  }
+
+  showNames() {
+    const list = this.listOfUsers.map(user => user.first_name + ' ' + user.last_name);
+    console.log(list);
+    return this;
+  }
+
+  showById(id) {
+    const itemId = this.listOfUsers.find(user => user.id === id);
+    if (this.listOfUsers.indexOf(itemId) == -1) {
+      console.log(`Unable to find user with id: ${id}`);
+    } else {
+      console.log(itemId);
+    }
+    return this;
+  }
+
+  add(obj) {
+    obj = {
+      "id": this.listOfUsers.length + 1,
+      "first_name": obj.first_name,
+      "last_name": obj.last_name || null,
+      "gender": obj.gender || null,
+      "language": obj.language || null
+    }
+    if (obj.first_name.length) {
+      this.listOfUsers.push(obj);
+      console.log(`Hi everyone, i am ${obj.first_name}`)
+    } else {
+      console.log(`please, enter first name `)
+    }
+    return this;
+  }
+
+  removeById(id) {
+    const item = this.listOfUsers.find(user => user.id === id);
+    let index = this.listOfUsers.indexOf(item);
+    if (index == -1) {
+      console.log(`Unable to find user with id: ${id}`);
+    } else {
+      this.listOfUsers.splice(index, 1);
+      console.log(`bye bey ${item.first_name}`)
+    }
+    return this;
+  }
+
+  logUsersCount() {
+    let count = this.listOfUsers.length;
+    console.log(`the total user count is ${count}`);
+    return this;
+  }
+}
